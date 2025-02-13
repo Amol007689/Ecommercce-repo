b039a69acd0963fcd36bd5d86b115f5399463982
Amol007689 <dimpuamol15@gmail.com>
2025-02-13 11:46:24 +0530

    message

--- backend/src/controller/product.js
+++ backend/src/controller/product.js
@@ -1,17 +1,19 @@
 const {Router} = require('express');
-const { productUpload } = require('../../multer');
-const productRouter = Router();
+const { productupload } = require('../../multer');
+const productModel = require('../Model/Productmodel');
+const productrouter = Router();
+const path=require('path')
 
-
-productRouter.get("/get-product", async(req, res) => {
-    // res.send('Product Route');
+productrouter.get("/get-product", async (req, res) => {
     try{
-        const productfind = await productModel.find();
+       const productfind= await productModel.find();
+         console.log(productfind);
         if(!productfind){
-            return res.status(400).json({message: 'No product found'});
+            return res.status(400).json({message:"No products found"});
         }
-        const products = productfind.map(product => {
-            return{
+        const products = productfind.map((product) => {
+            return {
+                id: product._id,
                 name: product.name,
                 description: product.description,
                 category: product.category,
@@ -20,45 +22,99 @@ productRouter.get("/get-product", async(req, res) => {
                 stock: product.stock,
                 email: product.email,
                 images: product.images,
-                createdAt: product.createdAt
-            }
+                createdAt: product.createdAt,
+
+            };
         });
-        res.status(200).json({products: products});
-    }catch{
-        console.error(err);
+
+        return  products
+        
     }
-})
+    catch(err){
+        console.log(err);
+    }
+});
 
-productRouter.post('/post-product',productUpload.array('files'),async (req, res) => {
-    const{name, description, category, tags, stock, email, price} = req.body;
-    const images = req.files.map(file => file.path);
+productrouter.post("/post-product",productupload.array('files'),async(req, res) => {
+    const {name, description, category, tags, price, stock, email} = req.body;
+    const images = req.files.map((file) => file.path);
     try{
-        const seller = await productModel.findOne({email: email});
+        const product = await productModel.findone({email:email});
         if(!seller){
-            return res.status(400).json({message: 'Seller not found'});
+            return res.status(400).json({message:"Seller not found"});
         }
-
-        if(images.length < 1){
-            return res.status(400).json({message: 'Please upload at least one image'});
+        if(images.length === 0){
+            return res.status(400).json({message:"Please upload atleast one images"});
         }
-
-        const newProduct = await productModel.create({
+        await productModel.create({
             name:name,
             description:description,
             category:category,
             tags:tags,
-            stocks:stock,
-            email:email,
             price:price,
+            stock:stock,
+            email:email,
             images:images
         });
-        res.status(201).json({message: 'Product created successfully', product: newProduct
-    });
-}
-    catch(error){
-        console.error(error);
+    }
+    catch(err){
+        console.log(err);
+    }
+    res.status(200).json({message:"Product added successfully"});
+
+});
+
+productrouter.put('/edit-product/:id',productupload.array('files',10),async(req,res)=>{
+
+    try{
+    const {id}=req.params
+    console.log(id)
+    const {name, description, category, tags, price, stock, email} = req.body;
+    const existproduct=await productModel.findById(id)
+
+    if(!existproduct){
+        res.status(400).json({message:"product does not exist"})
+    }
+    const updateimages=existproduct.images
+    if(req.files && req.files.length>0){
+        updateimages=req.files.map((img)=>{
+            return `/product/${path.basename(img.path)}`
+        })
+    }
+    existproduct.name=name
+    existproduct.description=description
+    existproduct.category=category
+    existproduct.tags=tags
+    existproduct.price=price
+    existproduct.stock=stock
+    existproduct.email=email
+    existproduct.images=updateimages
+
+   await existproduct.save()
+
+    res.status(200).json({product:existproduct})
+
+    }
+    catch(err){
+        console.log('error in updating')
+    }
+
+})
+
+productrouter.delete('/delete-product/:id',async(req,res)=>{
+    try{
+        const {id}=req.params
+        const existproduct=await productModel.findById(id)
+
+        if(!existproduct){
+            res.status(400).json({message:"product does not exist"})
+        }
+
+        await existproduct.deleteOne()
+
+    }catch(err){
+        console.log('error in delete')
     }
 })
 
-
-module.exports = productRouter;
+module.exports = productrouter;
